9c6562e831b9adb888720dfc4bc2b932477c565e
dacicus75 <dacicus75@hotmail.com>
2019-04-22 22:16:11 -0400

    minor changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
 {
     "name": "Jicaron Mandin",
-    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "profilePic": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
     "scores": [
     "1",
     "1",
